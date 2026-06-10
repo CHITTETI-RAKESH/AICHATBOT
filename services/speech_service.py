e222dfd5c25f2bdb23b2c70cde06816d340bf7d0
CHITTETI-RAKESH <chittetirakesh2@gmail.com>
2026-06-10 19:29:56 +0530

    Create speech_service.py

---
+++ services/speech_service.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+def speech_to_text():
+
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+
+        audio = recognizer.listen(source)
+
+    try:
+        return recognizer.recognize_google(audio)
+
+    except:
+        return ""
